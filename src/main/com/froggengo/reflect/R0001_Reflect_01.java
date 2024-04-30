e5cfa61fd66944eb54da89491a1e607810b37560
fanglile <fanglile@tctech.onaliyun.com>
2024-04-30 11:30:24 +0800

    获取方法的参数名

--- src/main/com/froggengo/reflect/R0001_Reflect_01.java
+++ src/main/com/froggengo/reflect/R0001_Reflect_01.java
@@ -4,6 +4,12 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 
 /**
+ * 1.idea 编译自带-g，借助asm读取字节码的本地变量表，可以读取方法名
+ * 2.maven compile插件默认-g，同上
+ * 3.-parameters 可以直接反射从方法名中获取方法名
+ * 4.接口无法借助asm读取本地变量表，-parameters也可以获取接口的方法名，但接口的参数名大多数无意义，因为子类可以修改
+ * https://www.cnblogs.com/yourbatman/p/11532689.html
+ * https://blog.csdn.net/qq_33535433/article/details/118106567
  * @author fly
  * @create 2024-04-29-18:24
  **/
