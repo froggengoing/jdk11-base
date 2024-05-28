860ad5ff3dffde3a930dc45d069810ad3150836b
fanglile <fanglile@tctech.onaliyun.com>
2024-05-29 06:53:08 +0800

    finally

--- src/main/com/froggengo/a_basic/T1_Finally.java
+++ src/main/com/froggengo/a_basic/T1_Finally.java
@@ -10,6 +10,7 @@ public class T1_Finally {
     public static void main(String[] args) {
         System.out.println(cal() == 15);
         System.out.println(cal1() == 10);
+        System.out.println(cal3() == 15);
         System.out.println(cal1_1().get() == 15);
         System.out.println(cal2_1().get() == 15);
     }
@@ -39,6 +40,20 @@ public class T1_Finally {
         }
     }
 
+    public static int cal3() {
+        int a = 5;
+        try {
+            a += 5;
+            return a;
+        } finally {
+            // 先执行
+            a += 5;
+            //可能JVM认为一个方法里面有两个return语句并没有太大的意义，所以try中的return语句给忽略了，
+            // 直接起作用的是最后finally中的return语句，就又重新形成了一条返回路径
+            return a;
+        }
+    }
+
     public static AtomicInteger cal1_1() {
         AtomicInteger atom = new AtomicInteger(5);
         try {
@@ -60,4 +75,6 @@ public class T1_Finally {
         }
 
     }
+
+
 }
