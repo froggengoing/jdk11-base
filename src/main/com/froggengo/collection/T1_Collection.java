0df925e094291f6faf3170f0f771c5de656ea6dd
fanglile <fanglile@tctech.onaliyun.com>
2024-06-11 16:24:57 +0800

    hashmap可以put(null,null)值,而ConcurrentHashMap不能

---
+++ src/main/com/froggengo/collection/T1_Collection.java
@@ -0,0 +1,62 @@
+package com.froggengo.collection;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author fly
+ * @create 2024-06-11-15:09
+ **/
+public class T1_Collection {
+    @Test
+    public void test11() {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        System.out.println(list.remove((Integer) 1));
+    }
+
+    /**
+     * 二义性
+     *  map.get("test")都为空，两种情况
+     * 1. 一种是value为null
+     * 2. 一种是没有test这个key
+     * 二义性通过containsKey判断
+     * 而在
+     */
+    @Test
+    public void test21() {
+        Map<String, String> map = new HashMap<>();
+        String test = map.get("test");
+        System.out.println(test);
+        System.out.println("map.containsKey(\"test\"):" + map.containsKey("test"));
+        map.put("test", null);
+        String test2 = map.get("test");
+        System.out.println(test2);
+        System.out.println("map.containsKey(\"test\"):" + map.containsKey("test"));
+
+        ConcurrentHashMap<String, String> conMap = new ConcurrentHashMap<>();
+        conMap.put("test",null);
+    }
+
+    /**
+     * hashmap可以put(null,null)值
+     */
+    @Test
+    public void test20() {
+        Map<String, String> map = new HashMap<>();
+        System.out.println("map.containsKey(null):" + map.containsKey(null));
+        // HashMap底层是Node<k,v>[] table
+        // 这里的key为null，只是说Node元素的key为null，而hashmap内部已经将key为null换算为0
+        String put = map.put(null, "1");
+        String s = map.get(null);
+        System.out.println("s.equals(\"1\"):" + s.equals("1"));
+        map.put(null, "2");
+        String s2 = map.get(null);
+        System.out.println("s.equals(\"2\"):" + s2.equals("2"));
+        System.out.println("map.containsKey(null):" + map.containsKey(null));
+    }
+}
